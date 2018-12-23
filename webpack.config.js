b51a3e56a895559362ed23bae2f6fcb54c3c0887
clicktravel-charlotteneill <charlie_neill@hotmail.com>
2018-12-23 11:59:33 +0000

    Can run javascript code

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // Example setup for your project:
+    // The entry module that requires or imports the rest of your project.
+    // Must start with `./`!
+    entry: '/Users/charlotteneill/flashEm/src/entry',
+    // Place output files in `./dist/my-app.js`
+    output: {
+        path: '/Users/charlotteneill/flashEm/dist',
+        filename: 'my-app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.json$/,
+                loader: 'json'
+            }
+        ]
+    }
+};
