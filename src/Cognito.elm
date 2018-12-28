cc0edb3cabe5231c509b3612a8f35f3282a0ce01
clicktravel-charlotteneill <charlie_neill@hotmail.com>
2018-12-28 23:01:10 +0000

    removed unused files

--- src/Cognito.elm
+++ src/Cognito.elm
@@ -2,6 +2,6 @@ port module Cognito exposing (signup, errors, signupSuccess)
 
 port signup : { emailAddress: String, password: String, username: String } -> Cmd msg
 
-port errors : (String -> msg) -> Sub msg  --JS is gonna send a string and we'll receive it in Elm as a subscription
+port errors : (String -> msg) -> Sub msg
 
 port signupSuccess : ({ username: String} -> msg) -> Sub msg
